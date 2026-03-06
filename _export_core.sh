9f3800218718721240404383842f75adb0cbd3d2
Mohcine Tor <mohcine.tor@cosmotech.com>
2026-03-06 14:10:45 +0100

    fix _export_core.sh script

--- _export_core.sh
+++ _export_core.sh
@@ -1,5 +1,7 @@
 terraform -chdir=$PWD/$1 output -json | jq -r '
 to_entries[]
 | select(.value.sensitive != true)
-| "export TF_VAR_\(.key)=\"\(.value.value // "")\""
+| .key as $k
+| ($k | sub("^out_"; "")) as $k2
+| "export TF_VAR_\($k2)=\"\(.value.value // "")\""
 ' > $PWD/out_core_infra.txt
